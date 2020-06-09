75114a0915d4b2c07c3dbf2b89d403b4e4961617
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-09 13:49:18 +0300

    final version of rout

--- routes/shop.js
+++ routes/shop.js
@@ -7,17 +7,17 @@ Router.get('/',shopController.getIndex ) ;
 
 Router.get('/products',shopController.getProducts ) ;
 
- Router.get('/products/:productId',shopController.productId);
+Router.get('/products/:productId',shopController.productId);
 
-/*Router.get('/cart',shopController.getCart ) ;
+Router.get('/cart',shopController.getCart ) ;
 
 Router.post('/cart', shopController.postCart);
 
 Router.post('/cart-delete-item', shopController.postCartDelete);
+
 Router.post('/create-order', shopController.postOrder);
 
-
-Router.get('/orders',shopController.getOrder) ;  */
+Router.get('/orders',shopController.getOrder) ;   
 
 //Router.get('/checkout',shopController.getCheckout ) ;
 
