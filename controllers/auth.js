fe3b4e2ecb33fa2c3f86ded2ecf80870c2ceafba
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-11 17:57:16 +0300

    added two modules for logging

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,34 @@
+const User = require('../models/user')
+
 exports.getLogin =(req,res,next )=>{
+   /*  const isLoggedIn =req.get('Cookie')
+    .trim()
+    .split('=')[1] === 'true'; */
+    console.log(req.session.isLoggedIn);
+    console.log(req.session.user);
     res.render('auth/login',{
         path: '/login',
         docTitle: 'Login',
+        isAuthenticated: false
     })
 
+}
+exports.postLogin= (req,res,next)=>{
+            
+    User.findById('5ede4f2d46955c1d107dab58')
+    .then(user => {
+      req.session.user = user
+      req.session.isLoggedIn = true;  
+      res.redirect('/');
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    
+   
+}
+exports.postLogout =(req,res,next)=>{
+    req.session.destroy(()=>{
+        res.redirect('/');
+    });
 }
