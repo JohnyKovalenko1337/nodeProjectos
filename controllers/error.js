031b37bad2efb2f8de6524c55b3cf11432acac8b
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-11 17:55:00 +0300

    added session for controller

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,3 @@
 exports.prob = (req,res,next) => {
-    res.status(404).render('404', {docTitle:'error',path:'404'}); //middleware for 404.html
+    res.status(404).render('404', {docTitle:'error',path:'404',isAuthenticated: req.session.isLoggedIn}); //middleware for 404.html
 }
