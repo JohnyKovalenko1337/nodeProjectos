813d5c16e1d0d3bf7c411cca04acf15faf17b1e7
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-12 22:26:58 +0300

    small postOrder  module change

--- controllers/shop.js
+++ controllers/shop.js
@@ -36,7 +36,8 @@ exports.getIndex =(req,res,next )=>{
         {prods: products,
         docTitle:'Shop',                           //render templates called shop
         path:'/', 
-        isAuthenticated: req.session.isLoggedIn
+        isAuthenticated: req.session.isLoggedIn,
+        csrfToken : req.csrfToken()
         }); 
     })
     .catch(err=>{
@@ -97,7 +98,7 @@ exports.postOrder= (req,res,next)=>{
         });
         const order = new Order({
             user:{
-                name: req.user.name,
+                email: req.user.email,
                 userId: req.user,
             },
             products:products
