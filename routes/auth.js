25feedd4e3e2c3d242033be62841af79ff15e635
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-12 22:45:20 +0300

    added post/get signup routers

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,11 @@ const authController = require('../controllers/auth');
 
 Router.get('/login',authController.getLogin);
 
+Router.get('/signup',authController.getSignup);
+
 Router.post('/login',authController.postLogin);
+
+Router.post('/signup',authController.postSignup);
 
 Router.post('/logout',authController.postLogout);
 
