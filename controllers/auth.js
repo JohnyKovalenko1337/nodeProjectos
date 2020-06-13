f7f557448343e884611be2890387a32be430a8d4
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-13 13:18:13 +0300

    new logic user feedback

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,10 +2,17 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 
 exports.getLogin =(req,res,next )=>{
+    let message = req.flash('error');           //check if message exist
+    if( message.length > 0){
+        message = message[0];
+    }
+    else{
+        message = null;             // if not exist we pass null instead of empty array
+    }
     res.render('auth/login',{
         path: '/login',
         docTitle: 'Login',
-        isAuthenticated: false
+        errorMessage: message     //pulling error message
     })
 
 }
@@ -16,7 +23,8 @@ exports.postLogin= (req,res,next)=>{
     User.findOne({email: email})
     .then(user => {
         if(!user){
-            return res.redirect('/login')
+            req.flash('error', 'Invalid email or password B)');
+            return res.redirect('/login');
         }
        bcrypt.compare(password,user.password)
             .then((doMatch)=>
@@ -29,6 +37,7 @@ exports.postLogin= (req,res,next)=>{
                     res.redirect('/');
                 });     
                }
+               req.flash('error', 'Invalid email or password B)');
                res.redirect('login');
             })
             .catch(err=>{
@@ -49,38 +58,60 @@ exports.postLogout =(req,res,next)=>{
 }
 
 exports.getSignup = (req,res,next)=>{
+    let message = req.flash('error');           //check if message exist
+    if( message.length > 0){
+        message = message[0];
+    }
+    else{
+        message = null;             // if not exist we pass null instead of empty array
+    }
     res.render('auth/signup',{
-        path: '/sign up',
+        path: '/signup',
         docTitle: 'SignUp',
-        isAuthenticated: false
+        errorMessage: message
     })
 }
 exports.postSignup=(req,res,next)=>{
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
-    User.findOne({email: email})
-    .then(userDoc=>{
-        if(userDoc){
-            return res.redirect('/signup');
+    User.findOne({ email: email })
+      .then(userDoc => {
+        if (userDoc) {
+          req.flash('error', 'E-Mail exists already, please pick a different one.');
+          return res.redirect('/signup');
         }
         return bcrypt
-            .hash(password, 12)
-            .then(hashedPassword=>{
-                const user = new User({
-                    email: email,
-                    password: hashedPassword,
-                    cart:{items:[]}
-                });
-                return user.save();
+          .hash(password, 12)
+          .then(hashedPassword => {
+            const user = new User({
+              email: email,
+              password: hashedPassword,
+              cart: { items: [] }
             });
-    })
-    
-    .then(result=>{
-        res.redirect('/login')
-    })
-    .catch((err)=>{
+            return user.save();
+          })
+          .then(result => {
+            res.redirect('/login');
+          });
+      })
+      .catch(err => {
         console.log(err);
-    });
+      });
     
 };
+
+exports.getReset = (req,res,next)=>{
+    let message = req.flash('error');           //check if message exist
+    if( message.length > 0){
+        message = message[0];
+    }
+    else{
+        message = null;             // if not exist we pass null instead of empty array
+    }
+    res.render('auth/reset',{
+        path: '/reset',
+        docTitle: 'Reset',
+        errorMessage: message     //pulling error message
+    })
+}
