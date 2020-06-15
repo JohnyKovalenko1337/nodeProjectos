624edd5fed500a84df03b0cbbc7a74a1ea79e501
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-15 18:56:42 +0300

    added validition to post routs

--- routes/admin.js
+++ routes/admin.js
@@ -2,22 +2,58 @@ const express = require('express');
 const path = require('path');
 const rootDir = require('../util/path');
 
+const {body} = require('express-validator');
 const Router= express.Router();
 
 const isAuth = require('../middleware/is-auth');
 
 const adminController = require('../controllers/admin');
-// /admin/add-product/ => /get/
-Router.get('/add-product', isAuth,  adminController.getAddProduct ) ;
-// /admin/add-product/ => /post
-Router.post('/add-product',isAuth, adminController.postAddProduct) ;
-// /admin/products/ => /get/
+
+
+Router.get('/add-product',  isAuth,  adminController.getAddProduct ) ;
+
+Router.post('/add-product',
+    [
+        body('title')
+            .isAlphanumeric()
+            .isLength({ min:2 })
+            .trim(),
+        body('imageUrl')
+            .isURL(),
+        body('price')
+            .isFloat(),
+        body('description')
+            .isAlphanumeric()
+            .isLength({ max:100 })
+            .trim()
+    ],
+    isAuth,
+    adminController.postAddProduct) ;
 
 Router.get('/products', isAuth,  adminController.getProducts) ;
 
-Router.get('/edit-product/:ProductId', isAuth,  adminController.getEditProduct) ;
+Router.get('/edit-product/:ProductId', isAuth, adminController.getEditProduct) ;
 
-Router.post ('/edit-product',isAuth, adminController.postEditProduct);
+Router.post ('/edit-product',
+    [
+        body('title')
+            .isString()
+            .isLength({ min:3 })
+            .trim(),
+        body('imageUrl')
+            .isURL(),
+        body('price')
+            .isFloat(),
+        body('description')
+            .isString()
+            .isLength({ max:100 })
+            .trim()
+    ],
+    isAuth,
+    adminController.postEditProduct);
 
 Router.post('/delete-product', isAuth,  adminController.postDeleteProduct) ; 
+
+
+
 module.exports=Router;
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const isAuth = require('../middleware/is-auth');
+
+const User = require('../models/user');
+const {check,body} = require('express-validator'); //destuctering by taking check func 
 
 const Router= express.Router();
 
@@ -19,7 +21,26 @@ Router.post('/reset',authController.postReset);
 
 Router.post('/login',authController.postLogin);
 
-Router.post('/signup',authController.postSignup);
+Router.post('/signup',
+    [
+        check('email').isEmail().withMessage('Please input valid email').custom((value,{req})=>{
+            return User.findOne({ email: value })
+            .then(userDoc => {
+                if (userDoc) {
+                return Promise.reject('E-Mail exists already, please pick a different one.') //  async validation
+                }
+            })
+        }),
+        body('password','Please enter password with only text and numbers at least 5 symbols')
+            .isLength({min:5}).isAlphanumeric(),
+        body('confirmPassword').custom((value,{req})=>{
+            if(value !== req.body.password){
+                throw new Error('Passwords have to match');
+            }
+            return true;
+        })
+    ],
+  authController.postSignup);
 
 Router.post('/logout',authController.postLogout);
 
