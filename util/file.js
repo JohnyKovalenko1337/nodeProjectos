37fc3302fd989bdd7e3967ec9ae080def7da6975
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-17 14:44:07 +0300

    added func for deleting files

---
+++ util/file.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const deleteFile = (filePath)=>{
+    fs.unlink(filePath,(err)=>{
+        if(err){
+            throw new Error(err);
+        }
+    });
+}
+exports.deleteFile = deleteFile;
